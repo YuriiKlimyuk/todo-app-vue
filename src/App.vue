b7e51ae34c547e9969bdc6461d2be1b95a223c78
klymiuk yurii <yu.ye.klymiuk@nuwm.edu.ua>
2024-04-02 11:25:50 +0300

    Виконано реалізацію веб-застосування

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+ <HomePage></HomePage>
+</template>
+
+<script>
+import HomePage from './views/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #2F035D;
+}
+</style>
